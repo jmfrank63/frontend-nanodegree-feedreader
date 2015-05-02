e30f8ad6b25f1fcc76f16484051821d0c0cda7f4
Johannes Frank <jmfrank63@gmail.com>
2015-05-02 09:39:07 +0000

    technically things are working

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -91,18 +91,39 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+            loadFeed(0, done);
+         });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+         it('Check entry in feed container', function(done) {
+             expect($('.feed').find('article')).toHaveClass('entry');
+             done();
+         });
 
+    });
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         var content;
+
+         beforeEach(function(done) {
+                loadFeed(1, done);
+                content = $('.feed').find('h2').text();
+         });
+
+         it('Check if content changes', function(done) {
+             loadFeed(0, done);
+             expect($('.feed').find('h2').text()).not.toEqual(content);
+         });
+    });
 }());
